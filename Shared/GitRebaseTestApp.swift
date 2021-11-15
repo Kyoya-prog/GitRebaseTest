a037e6053b21d2820333a87c6bf9b5fec913800c
Kyoya-prog <kyoyaprog@gmail.com>
2021-11-16 08:59:35 +0900

    Initial Commit

---
+++ Shared/GitRebaseTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitRebaseTestApp.swift
+//  Shared
+//
+//  Created by kyoya.matsuyama on 2021/11/16.
+//
+
+import SwiftUI
+
+@main
+struct GitRebaseTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

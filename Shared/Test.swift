54bea880836b4b8b410867640b1ff93b724123ef
Kyoya-prog <kyoyaprog@gmail.com>
2021-11-16 09:03:31 +0900

    testクラスを追加

---
+++ Shared/Test.swift
@@ -0,0 +1,14 @@
+//
+//  Test.swift
+//  GitRebaseTest
+//
+//  Created by kyoya.matsuyama on 2021/11/16.
+//
+
+import Foundation
+
+class Test{
+    func test()->Int{
+        return 2 * 2 * 2
+    }
+}
